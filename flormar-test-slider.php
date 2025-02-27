115ab999eb9b2f18b62dc44ded4eecc459728062
Maksim Nizovyi <nizovoy998@gmail.com>
2025-02-27 14:30:29 +0200

    enqueue style/scripts

--- flormar-test-slider.php
+++ flormar-test-slider.php
@@ -10,6 +10,21 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+function flormar_test_slider_enqueue_scripts()
+{
+    wp_enqueue_style('slick-slider', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css');
+    wp_enqueue_style('slick-theme', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css');
+
+    wp_enqueue_style('flormar-test-slider', plugins_url('assets/css/slider.css', __FILE__));
+
+    wp_enqueue_script('slick-slider', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array('jquery'), '1.8.1', true);
+
+    wp_enqueue_script('flormar-test-slider', plugins_url('assets/js/slider-init.js', __FILE__), array('jquery', 'slick-slider'), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'flormar_test_slider_enqueue_scripts');
+
+add_shortcode('flormar-test-slider', 'flormar_test_slider_shortcode');
+
 function flormar_test_slider_activate()
 {
     if (!class_exists('WooCommerce')) {
