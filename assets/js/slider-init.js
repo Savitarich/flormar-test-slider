ab6e7035179051bac3786d8724775f0737702202
Maksim Nizovyi <nizovoy998@gmail.com>
2025-02-27 15:00:04 +0200

    added main shortcode

---
+++ assets/js/slider-init.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($){
+    $('.flormar-test-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+        ]
+    });
+});
--- flormar-test-slider.php
+++ flormar-test-slider.php
@@ -10,6 +10,69 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+function flormar_test_slider_shortcode($atts)
+{
+    $atts = shortcode_atts(array(
+        'max-price' => '',
+        'min-price' => '',
+    ), $atts, 'flormar-test-slider');
+
+    $meta_query = array();
+    if (! empty($atts['min-price'])) {
+        $meta_query[] = array(
+            'key'     => '_price',
+            'value'   => $atts['min-price'],
+            'compare' => '>=',
+            'type'    => 'NUMERIC'
+        );
+    }
+    if (! empty($atts['max-price'])) {
+        $meta_query[] = array(
+            'key'     => '_price',
+            'value'   => $atts['max-price'],
+            'compare' => '<=',
+            'type'    => 'NUMERIC'
+        );
+    }
+
+    $args = array(
+        'post_type'      => 'product',
+        'posts_per_page' => -1,
+    );
+
+    if (! empty($meta_query)) {
+        $args['meta_query'] = $meta_query;
+    }
+
+    $query = new WP_Query($args);
+
+    if (! $query->have_posts()) {
+        return '<p>' . esc_html__('No products found.', 'flormar-test-slider') . '</p>';
+    }
+
+    ob_start(); ?>
+
+    <div class="flormar-slider-container">
+        <h3> <?= __('Best-selling products', 'flormar-test-slider') ?></h3>
+        <div class="flormar-test-slider">
+            <?php while ($query->have_posts()) : $query->the_post();
+                global $product; ?>
+                <div class="slider-item">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php echo woocommerce_get_product_thumbnail(); ?>
+                        <h3 class="product-title"><?php the_title(); ?></h3>
+                        <span class="price"><?php echo $product->get_price_html(); ?></span>
+                    </a>
+                </div>
+            <?php endwhile; ?>
+        </div>
+    </div>
+
+<?php
+    wp_reset_postdata();
+    return ob_get_clean();
+}
+
 function flormar_test_slider_enqueue_scripts()
 {
     wp_enqueue_style('slick-slider', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css');
