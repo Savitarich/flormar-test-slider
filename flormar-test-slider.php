0df11b2ede81d19d2cf34c008ee5ab06aa74685d
Maksim Nizovyi <nizovoy998@gmail.com>
2025-02-27 14:08:44 +0200

    register activation / deactivation

--- flormar-test-slider.php
+++ flormar-test-slider.php
@@ -9,3 +9,19 @@
 if (!defined('ABSPATH')) {
     exit;
 }
+
+function flormar_test_slider_activate()
+{
+    if (!class_exists('WooCommerce')) {
+        deactivate_plugins(plugin_basename(__FILE__));
+        wp_die('This plugin requires WooCommerce to be installed and activated.');
+    }
+    flush_rewrite_rules();
+}
+register_activation_hook(__FILE__, 'flormar_test_slider_activate');
+
+function flormar_test_slider_deactivate()
+{
+    flush_rewrite_rules();
+}
+register_deactivation_hook(__FILE__, 'flormar_test_slider_deactivate');
